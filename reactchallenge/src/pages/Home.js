05e0a2f3f194a59a999c4caaf36efbd648a27853
mateogioia <mategioia@gmail.com>
2023-06-29 15:03:48 -0300

    Home screen first version

--- reactchallenge/src/pages/Home.js
+++ reactchallenge/src/pages/Home.js
@@ -1,7 +1,24 @@
+import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import OpenWithOutlinedIcon from '@mui/icons-material/OpenWithOutlined';
+import { Fab, Stack } from '@mui/material';
+
 function Home() {
-    return (
-        <div>Home</div>
-    )
+  return (
+    <div style={{ backgroundColor: "#F5F5F5" }}>
+      <Stack spacing={0}>
+        <Fab onClick={() => {console.log("+ button clicked")}} color="primary" aria-label="zoom-in">
+          <AddOutlinedIcon />
+        </Fab>
+        <Fab onClick={() => {console.log("- button clicked")}} color="primary" aria-label="zoom-out">
+          <RemoveOutlinedIcon />
+        </Fab>
+      </Stack>
+      <Fab onClick={() => {console.log("Expand button clicked")}} color="primary" aria-label="expand">
+        <OpenWithOutlinedIcon />
+      </Fab>
+    </div>
+  );
 }
 
 export default Home;
